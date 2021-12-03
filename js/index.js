46c638700468bfdc0601978d5a13b0d2b519fb3b
Stanislav <stanislavasilevich@gmail.com>
2021-12-03 18:01:39 +0300

    feat: добавил активные точки перехода по секциям в верстке

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const point = document.querySelectorAll('.points li');
+
+// const elem = document.querySelector('.scroll');
+
+// document.addEventListener('scroll', function() {
+//   const posTop = elem.getBoundingClientRect().top;
+  
+//   // Блок достиг верхней границы экрана (или выше)
+//   // elem.classList.toggle('visible', posTop <= 0);
+  
+//   // Блок только появляется снизу (или выше)
+//   // elem.classList.toggle('visible', posTop < window.innerHeight);
+  
+//   // Блок целиком находится в видимой зоне
+//   // elem.classList.toggle('visible', posTop + elem.clientHeight <= window.innerHeight && posTop >= 0);
+
+//   console.log('posTop: ', posTop);
+// });
