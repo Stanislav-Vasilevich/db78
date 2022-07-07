5ce1eb2495c4b55a8107eb5038d9b1a02086fd28
Stanislav <stanislavasilevich@gmail.com>
2022-07-07 21:40:05 +0300

    сверстал страницу контактов

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -2,21 +2,16 @@
 window.addEventListener('scroll', () => {
   let scrollDistance = window.scrollY;
 
-  // для экранов более 900
-  if (window.innerWidth > 900) {
-    document.querySelectorAll('.section').forEach((el, i) => {
+  document.querySelectorAll('.section').forEach((el, i) => {
 
-      if (el.offsetTop - document.querySelector('.nav').clientHeight <= scrollDistance) {
-        document.querySelectorAll('.nav a').forEach((el) => {
-          if (el.classList.contains('active')) {
-            el.classList.remove('active');
-          }
-        });
+    if (el.offsetTop - document.querySelector('.nav').clientHeight <= scrollDistance) {
+      document.querySelectorAll('.nav a').forEach((el) => {
+        if (el.classList.contains('active')) {
+          el.classList.remove('active');
+        }
+      });
 
-        console.log(i);
-
-        document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('active');
-      }
-    });
-  }
+      document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('active');
+    }
+  });
 });
